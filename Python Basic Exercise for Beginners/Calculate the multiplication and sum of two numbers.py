69254ecfcbbfba1aea7ea5a6adb13ead70a697b2
AndrijLet <selektawaj@gmail.com>
2025-11-07 22:47:57 +0200

    071125

---
+++ Python Basic Exercise for Beginners/Calculate the multiplication and sum of two numbers.py
@@ -0,0 +1,15 @@
+number1 = 40
+number2 = 30
+
+product = number1 * number2
+
+if product <= 1000:
+    result = product
+
+else:
+    result = number1 + number2
+
+print("The result is", result)
+
+
+
---
+++ Python Basic Exercise for Beginners/Please enter a number number.py
@@ -0,0 +1,9 @@
+# Please enter a number
+number = int (input("Enter a number: "))
+
+#check if it is even
+if number % 2 == 0:
+    print("Even number.")
+
+else:
+    print("Odd number.")
