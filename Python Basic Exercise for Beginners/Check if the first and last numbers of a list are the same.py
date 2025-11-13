648bdd26c87a8bb2851058fe3577d486b532ed1a
AndrijLet <selektawaj@gmail.com>
2025-11-13 19:56:37 +0200

    13112025

---
+++ Python Basic Exercise for Beginners/Check if the first and last numbers of a list are the same.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 40, 10]
+
+if numbers[0] == numbers[-1]:
+    print("True")
+else:
+    print("False")  
---
+++ Python Basic Exercise for Beginners/Display numbers divisible by 5.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 33, 45, 54, 55]
+
+print("Числа які діляться на 5:")
+
+for num in numbers:
+    if num % 5 == 0:
+        print(num)
