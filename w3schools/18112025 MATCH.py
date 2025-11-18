925f7770ba27b9734560281f9fa5a6adc644df89
AndrijLet <selektawaj@gmail.com>
2025-11-18 19:40:40 +0200

    18112025

---
+++ w3schools/18112025 MATCH.py
@@ -0,0 +1,27 @@
+# age = int(input("Enter your age: "))
+#
+# match age:
+    # case 18:
+    #     print("You are 18 years old")
+    # case 20:
+    #     print("You are 20 years old")
+#
+# mylist = [45, 26, "hi", 3, "hola", 32]
+#
+# result = 1
+# for item in mylist:
+#     if isinstance(item, int):
+#         result *= item
+# print(result)
+#
+# nevlist = [1,2,2,3,4,1,1,5,1]
+# newlist = [x for x in newlist if not in x]
+# print()
+
+def sum(num1,num2):
+    return num1 + num2
+
+
+# result = sum(2,3)
+# print(result)
+print(sum(3,9))
