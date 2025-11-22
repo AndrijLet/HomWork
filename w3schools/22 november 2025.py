c8bf927ce029d9149efaa9cbdf70a2ddec80ecc0
AndrijLet <selektawaj@gmail.com>
2025-11-22 12:25:02 +0200

    22 nov 2025 shool

---
+++ w3schools/22 november 2025.py
@@ -0,0 +1,35 @@
+# # def sum(num1, num2):
+# #     print(num1 + num2)
+#
+# # sum(1, 2)
+# #
+# # sum(23, 657)
+# def sum(num1, num2):
+#     return num1 + num2
+#
+# result = sum(23,52)
+#
+# print(sum(25,25))
+#
+# def sum():
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+# num3 = int(input("Enter another number: "))
+#
+
+def sum(num1,num2,num3):
+    return (num1+num2+num3)
+
+# print(sum(num1,num2,num3))
+
+#ще один спосіб для отримання від користувача
+
+print(sum(int(input("num1:")), int(input("num2:")), int(input("num3:"))))
+
+
+
+
+
+
+
