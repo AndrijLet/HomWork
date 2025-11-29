49647d581dc35d72e61f4ac7ba4266da97664840
AndrijLet <selektawaj@gmail.com>
2025-11-29 12:39:20 +0200

    29 nov 2025

---
+++ Python Basic Exercise for Beginners/29 nov 2025.py
@@ -0,0 +1,24 @@
+# list1 = [10, 20, 25, 30, 35]
+# list2 = [40, 45, 60, 75, 90]
+# list3 = []
+#
+# def numbers_od(list1, list2):
+#     for number in list1:
+#         if number % 2 != 0:
+#             list3.append(number)
+#     for number in list2:
+#         if number % 2 == 0:
+#             list3.append(number)
+# numbers_od(list1, list2)
+#
+# print(list3)
+
+#map(риймаємо функцію та масив)
+list1 = [10, 20, 25, 30, 35]
+# # for x in list1:
+#
+# def func(x):
+#     return x
+# new_arr = list(map())
+
+reduce
