60c9b0b02072447bd3dd457262eb88169d8c4295
AndrijLet <selektawaj@gmail.com>
2025-12-03 20:03:33 +0200

    03 dec 2025

---
+++ Python Basic Exercise for Beginners/03 dec 2025.py
@@ -0,0 +1,24 @@
+"""Створити вищу функцію, яка буде застосовувати операцію перетворення"""
+# def transform_list(data, operation):
+#     return [operation(item) for item in data]
+#
+# odd_or_even = lambda x: "even" if x % 2 == 0 else "odd"
+#
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 16, 25]
+#
+#
+# result = transform_list(list, odd_or_even)
+# print(result)
+
+"""Задача 3 — перетворити всі слова у списку на ВЕЛИКІ ЛІТЕРИ.
+Task 3 — convert all words in the list to UPPERCASE."""
+# def transform_list(data, operation):
+#     return [operation(item) for item in data]
+#
+# words = ["apple", "banana", "cherry"]
+#
+# to_upper = lambda w: w.upper()
+# result = transform_list(words, str.upper)
+# print(result)
+
+"""Task - 4"""
---
+++ w3schools/03 dec 2025 w3shools.py
@@ -0,0 +1,28 @@
+# print(type(range(10)))
+#
+# for i in range(10, 16):
+#     print(i, end=" ")
+
+# for i in range(10, 50, 5):
+#     print(i, end=" ")
+#
+# for i in range(9, 100, 3):
+#     print(i, end=" ")
+#
+# for i in range(1,10, 2):
+#     print("Current valute of i is", i)
+#
+# list1 = ['Jessa', 'Emma', 20, 30, 75.5]
+#
+# for i in range (len(list1)):
+#     print(list1[i])
+#
+
+"""Виведіть наступний числовий шаблон, використовуючи Python  range() та цикл."""
+# for num in range(11):
+#     for i in range(num):
+#         print(num, end=" ")
+#     print()
+
+# for num in range(5, -1, -1):
+#     # print(num)
