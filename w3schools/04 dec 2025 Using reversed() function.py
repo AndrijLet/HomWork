fe599d0b1f9e777d3a724358371b48092246d383
AndrijLet <selektawaj@gmail.com>
2025-12-04 20:38:37 +0200

    04 dec 2025

---
+++ w3schools/04 dec 2025 Using reversed() function.py
@@ -0,0 +1,35 @@
+# for i in reversed(range(0,11)):
+#     print(i, end=" ")
+
+# for i in reversed(range(1,11, 2)):
+#     print(i, end=" ")
+
+# list1 = [10, 20, 30, 40, 50]
+#
+# for i in range(len(list1) -1, -1, -1):
+#     print(list1[i], end=" ")
+
+# for i in range(30, 20, -2):
+#     print(i, end=" ")
+
+# for i in range(7, 100, 7):
+#     print(i, end=" ")
+
+# for i in range(2, -5, -1):
+#     print(i, end=", ")
+
+# sample_list = list(range(2, 10, 2))
+# print(type(sample_list))
+#
+# print(sample_list)
+#
+# for item in sample_list:
+#     print(item)
+
+# sample_list = list(range(10, 100, 10))
+#
+# for i in range(0, len(sample_list), 1):
+#     sample_list[i] = sample_list[i] * 2
+#
+# print(sample_list)
+
