4c31a9c3a4304dffb76c50556c1e74c643a12578
AndrijLet <selektawaj@gmail.com>
2025-12-19 21:54:54 +0200

    19 dec 2025 ex5 OOP

--- Python Basic Exercise for Beginners/Python Object-Oriented Programming/Python Object-Oriented Programming.py
+++ Python Basic Exercise for Beginners/Python Object-Oriented Programming/Python Object-Oriented Programming.py
@@ -50,18 +50,39 @@
 
 '''OOP Exercise 4: Class Inheritance'''
 
+# class Vehicle:
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+#     def seating_capacity(self, capacity):
+#         return f"The seating capacity of a {self.name} is {capacity} passengers"
+#
+# class Bus(Vehicle):
+#     def seating_capacity(self, capacity=50):
+#         return super().seating_capacity(capacity=50)
+#
+# School_bus = Bus("School Volvo", max_speed=180, mileage=12)
+# print(School_bus.seating_capacity())
+
+'''OOP Exercise 5: Define a property that must have the same value for every class instance (object)'''
+
 class Vehicle:
+    color = "White"
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
 
-    def seating_capacity(self, capacity):
-        return f"The seating capacity of a {self.name} is {capacity} passengers"
-
 class Bus(Vehicle):
-    def seating_capacity(self, capacity=50):
-        return super().seating_capacity(capacity=50)
+    pass
 
-School_bus = Bus("School Volvo", max_speed=180, mileage=12)
-print(School_bus.seating_capacity())
+class Car(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.color, School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+
+car = Car("Audi Q5", 240, 18)
+print(car.color, car.name, "Speed:", car.max_speed, "Mileage:", car.mileage)
